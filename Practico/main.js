6e1a72238116eceec96ed9432989aa92d7fb23e1
GustavoN <gustavojaviernunezdiaz@gmail.com>
2026-02-24 22:22:52 -0300

    Se comenzó el practico.

--- Practico/main.js
+++ Practico/main.js
@@ -1,12 +1,39 @@
-function obtenerArticulos(){
-    fetch('https://example.com/api/data', {
+
+    async function obtenerProductos() {
+try {
+const response = await fetch('https://602ee9da4410730017c51705.mockapi.io/api/v1/productos');
+const data = await response.json();
+console.log(data);
+return data;
+} catch (error) {
+console.error(error);
+}
+}
+obtenerProductos();
+
+
+async function agregarProducto() {
+    const prodPrueba = {    
+"nombre": "control remoto",
+ "precio": 45,
+ "imagen": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK0IZ6XNrGCiBhcISFqa2dwUTa46nZfhLu1Q&s",
+ "descripcion": "Para conejos",
+ "numero": 2254,
+ "titulo": "Zanahoria"};
+    try {
+const response = await fetch('https://602ee9da4410730017c51705.mockapi.io/api/v1/productos', {
 method: 'POST',
-body: JSON.stringify({ name: 'John Doe', email: 'john.doe@example.com' }),
+body: JSON.stringify(prodPrueba),
 headers: {
 'Content-Type': 'application/json'
 }
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+});
+const data = await response.json();
+console.log(data);
+} catch (error) {
+console.error(error);
 }
+
+}
+
+agregarProducto();
